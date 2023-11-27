c0ed5520eed20c80f023980aec0da6863dc1903a
deborah-powers <deborah.powers.dev@gmail.com>
2023-11-27 09:53:18 +0100

    boulot 11/27  9:53

---
+++ wef-immo/immo.js
@@ -0,0 +1,17 @@
+var agence ="";
+var agencePhone ="";
+var prix ="";
+var dpe ="";
+var ville ="";
+var quartier ="";
+var annexe ="";
+var etage ="";
+
+// https://www.seloger.com/annonces/achat/appartement/rueil-malmaison-92/plateau-mont-valerien/207723151.htm
+if (window.location.href.includes ('https://www.seloger.com/annonces/achat/appartement/')){
+	console.log ('oui');
+	const prixTag = document.body.findTag ('bold first');
+//	document.body.findTag ('showcase-description');
+//	document.body.findTag ('donRNt');
+//	document.body.simplifyNesting();
+}
