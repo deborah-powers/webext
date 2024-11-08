509d68c7c195858ccb10bc58a45d5f6a5413c5df
deborah-powers <deborah.powers.dev@gmail.com>
2024-11-08 17:47:52 +0100

    travail 11/08 17:47

--- rgaa/ana-all.js
+++ rgaa/ana-all.js
@@ -5,14 +5,6 @@ HTMLElement.prototype.verifyAttr = function (attrName){
 	else message = message + this.attributes[attrName].value;
 	return message;
 }
-HTMLElement.prototype.verifyRole = function (roleValue){
-	if (this.attributes['role'] !== undefined && this.attributes['role'].value === roleValue){
-		this.style.border = 'solid 4px deeppink';
-	}
-	else{
-		for (var c=0; c< this.children.length; c++) this.children[c].verifyRole;
-	}
-}
 HTMLElement.prototype.isinLink = function (roleValue){
 	if (this.tagName === 'BODY') return null;
 	else if (this.tagName === 'A' || (this.attributes['role'] !== undefined && this.attributes['role'].value === 'link')) return 'link';
@@ -20,12 +12,6 @@ HTMLElement.prototype.isinLink = function (roleValue){
 	else return this.parentElement.isinLink();
 }
 HTMLElement.prototype._addLabel = function(){
-	// identifier l'objet
-	this.style.border = 'solid 4px deeppink';
-	const container = this.isinLink();
-	if (container === 'link') this.style.borderStyle = 'dashed';
-	else if (container === 'button') this.style.borderStyle = 'dotted';
-	// ajouter le message
 	var label = document.createElement ('span');
 	/*
 	label.style.color = '#420';
@@ -40,18 +26,29 @@ HTMLElement.prototype._addLabel = function(){
 	label.innerHTML = label.innerHTML +'<br/>'+ this.verifyAttr ('aria-label');
 	label.innerHTML = label.innerHTML +'<br/>'+ this.verifyAttr ('aria-labelledby');
 	this.parentElement.insertBefore (label, this);
-	/*
-	this.addEventListener ('mouseover', function (event){
-		console.log (event.target.previousElementSibling);
+	this.addEventListener ('mouseenter', function (event){
 		event.target.previousElementSibling.style.display = 'block';
 		event.preventDefault();
-	}, false);
+	});
 	this.addEventListener ('mouseleave', function (event){
-		console.log (event.target.previousElementSibling);
 		event.target.previousElementSibling.style.display = 'none';
 		event.preventDefault();
-	}, false);
-	*/
+	});
 	return label;
 }
 HTMLElement.prototype.addLabel = function(){ return this._addLabel(); }
+HTMLElement.prototype._addBorder = function (bordColor){
+	var label = null;
+	this.style.border = 'solid 4px '+ bordColor;
+	const container = this.isinLink();
+	if (container === 'link') this.style.borderStyle = 'dashed';
+	else if (container === 'button') this.style.borderStyle = 'dotted';
+	else if (! container) label = this.addLabel();
+}
+HTMLElement.prototype.addBorder = function (bordColor){ this._addBorder ('deeppink'); }
+HTMLElement.prototype.verifyRole = function (roleValue){
+	if (this.attributes['role'] !== undefined && this.attributes['role'].value === roleValue) this._addBorder ('magenta');
+	else{
+		for (var c=0; c< this.children.length; c++) this.children[c].verifyRole (roleValue);
+	}
+}
--- rgaa/ana-image.js
+++ rgaa/ana-image.js
@@ -2,7 +2,8 @@ HTMLImageElement.prototype.addLabel = function(){
 	var label = this._addLabel();
 	label.innerHTML = label.innerHTML +'<br/>'+ this.verifyAttr ('alt');
 }
-document.body.verifyRole();
+document.body.verifyRole ('img');
+document.body.verifyRole ('image');
 var images = document.getElementsByTagName ('img');
-for (var i=0; i< images.length; i++) images[i].addLabel();
+for (var i=0; i< images.length; i++) images[i].addBorder();
 
