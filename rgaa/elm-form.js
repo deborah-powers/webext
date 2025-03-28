aad4c313f1ca9b013c96fd0382c05a8bfa3576b7
deborah-powers <deborah.powers.dev@gmail.com>
2025-03-28 17:32:55 +0100

    travail 03/28 17:32

--- rgaa/elm-form.js
+++ rgaa/elm-form.js
@@ -1,3 +1,8 @@
+HTMLElement.prototype.computeAccessibleName = function(){
+	if (this.getAttribute ('hidden')) return "";
+}
+
+
 function removeHighlight(){
 	var highlighted = document.getElementsByClassName ('rgaa-highlight');
 	for (var h=0; h< highlighted.length; h++) highlighted.classList.remove ('rgaa-highlight');
@@ -8,46 +13,31 @@ function getByFor (inputId){
 	const labels = document.getElementsByTagName ('label');
 	var labelId =-1;
 	for (var l=0; l< labels.length; l++){
-		if (inputId === labels[l].for) labelId =l;
+		if (inputId === labels[l].getAttribute ('for')) labelId =l;
 	}
 	if (labelId === -1) return null;
 	else return labels[labelId];
 }
-
 function colorLabel (event){
-	console.log ('colorLabel');
 	var highlighted = document.getElementsByClassName ('rgaa-highlight');
-	for (var h=0; h< highlighted.length; h++) highlighted.classList.remove ('rgaa-highlight');
-	console.dir (event.target);
-	event.target.labelTag.classList.add ('rgaa-highlight');
+	for (var h=0; h< highlighted.length; h++) highlighted[0].classList.remove ('rgaa-highlight');
+	for (var l=0; l< event.target.labels.length; l++) event.target.labels[l].classList.add ('rgaa-highlight');
+}
+function bleachLabel(){
+	var highlighted = document.getElementsByClassName ('rgaa-highlight');
+	for (var h=0; h< highlighted.length; h++) highlighted[0].classList.remove ('rgaa-highlight');
 }
 HTMLInputElement.prototype.verifyTitle = function(){
-	var titleExists = false;
-	if ([ 'submit', 'reset', 'button' ].includes (this.type) && this.id.isEmpty() &&! exists (this.value))
-		titleExists = HTMLElement.prototype.verifyTitle.call (this);
-	else if ([ 'submit', 'reset', 'button' ].includes (this.type) && exists (this.value)){
-		this.infos = this.infos + 'value: '+ this.value +'<br/>';
-		titleExists = true;
+	var intitule = this.getIntitule();
+	if (intitule.includes ('plusieurs labels') || intitule.includes ('rien: ')) this.classList.add ('rgaa-error');
+	if (this.labels.length >0){
+		this.addEventListener ('mouseover', colorLabel);
+		this.addEventListener ('mouseleave', bleachLabel);
 	}
-	else if (this.id.isEmpty()) titleExists = HTMLElement.prototype.verifyTitle.call (this);
-	else{
-		var label = getByFor (this.id);
-		console.log ('label o', this.id, label);
-		if (label){
-			console.log ('label a');
-			this.infos = this.infos + 'label: '+ label.innerText +'<br/>';
-			console.log ('label b');
-			this.labelTag = label;
-			console.log ('label c');
-			this.addEventListener ('mouseover', colorLabel);
-			console.log ('label d');
-			titleExists = true;
-		}
-		else titleExists = HTMLElement.prototype.verifyTitle.call (this);
-	}
-	if (! titleExists){
-		this.classList.add ('rgaa-error');
-		this.infos = this.infos.replace ('rien: rien', 'pas de nom accessible');
-}}
-const inputs = document.getElementsByTagName ('input');
-for (var l=0; l< inputs.length; l++) inputs[l].addAll();
+	intitule = intitule.replace ('rien: rien', 'pas de nom accessible');
+	this.infos = this.infos +'<br/>'+ intitule;
+}
+var fields = document.getElementsByTagName ('input');
+for (var l=0; l< fields.length; l++) fields[l].addAll();
+fields = document.getElementsByTagName ('textarea');
+for (var l=0; l< fields.length; l++) fields[l].addAll();
