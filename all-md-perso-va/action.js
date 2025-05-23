52646a8031b5e8fbd1540ceaaeceb35b15222925
deborah-powers <deborah.powers.dev@gmail.com>
2025-05-23 17:09:31 +0200

    travail 05/23 17:09

--- all-md-perso-va/action.js
+++ all-md-perso-va/action.js
@@ -5,34 +5,42 @@ var headPage =`
 	<meta charset='utf-8'/>
 	<base target='_blank'>
 	<link rel='icon' type='image/svg+xml' href='file:///C:/wamp64/www/site-dp/data/nounours-perso.svg'/>
-	<link rel='stylesheet' type='text/css' href='file:///C:/wamp64/www/site-dp/library-css/structure.css'/>
-	<link rel='stylesheet' type='text/css' href='file:///C:/wamp64/www/site-dp/library-css/perso.css'/>
-	<link rel='stylesheet' type='text/css' href='file:///C:/wamp64/www/site-dp/library-css/shapes.css'/>
 `;
 // récupérer les metadonnées de mes articles
 var header = "<h1><a href='$lien'>$titre</a></h1><p>par <a href='$lienAuteur'>$auteur</a></p><p>à propos de $sujet</p>";
-const title = window.location.href.findTitleFromUrl();
+// variable de import-js
+crutialData =`
+	exists: exists,
+	prepareText: prepareText,
+	findTitle: function (url){ return url.findTitleFromUrl(); }
+`;
+const htmlLib = callLibrary ([ 'textFct', 'htmlFct' ]);
+const title = htmlLib.findTitle (window.location.href);
 headPage = headPage.replace ('<title></title>', '<title>' + title + '</title>');
 document.head.innerHTML = headPage;
 
+const styleList =[ 'structure', 'perso', 'shapes' ];
+if ('http' === window.location.href.substring (0,4)) addStyle (styleList);
+else{ for (var c=0; c< styleList.length; c++) addCss (styleList[c]); }
+
 function findMetaLocal (metadata, title){
 	header = header.replace ('$titre', title);
-//	console.log (metadata, metadata['autlink'], exists (metadata['autlink']), metadata.autlink, exists);
+//	console.log (metadata, metadata['autlink'], htmlLib.exists (metadata['autlink']), metadata.autlink, htmlLib.exists);
 
 	if (""=== metadata['lien']){
 		header = header.replace ("<a href='$lien'>", "");
 		header = header.replace ('</a>', "");
 	}
 	else header = header.replace ('$lien', metadata['lien']);
-	if (! exists (metadata['autlink'])){
+	if (! htmlLib.exists (metadata['autlink'])){
 		header = header.replace ("<a href='$lienAuteur'>", "");
 		header = header.replace ('$auteur</a>', '$auteur');
 	}
 	else header = header.replace ('$lienAuteur', metadata['autlink']);
 	header = header.replace ('$auteur', metadata['auteur']);
 	header = header.replace ('$sujet', metadata['sujet']);
-	if (exists (metadata['date'])) header = header + '<p>date: '+ metadata['date'] + '</p>';
+	if (htmlLib.exists (metadata['date'])) header = header + '<p>date: '+ metadata['date'] + '</p>';
 	document.body.innerHTML = header + document.body.innerHTML;
 }
-const metadata = prepareText();
+const metadata = htmlLib.prepareText();
 findMetaLocal (metadata, title);
--- all-md-perso-va/readme.txt
+++ all-md-perso-va/readme.txt
@@ -1,4 +1,4 @@
 extension pour transformer des textes txt ou md en html.
 ces fichiers ont une mise en forme personnelle.
 l'extension devrait fonctionner en version 2 et 3. il faut juste adapter la version selon le navigateur où je veux installer mon extension.
-exemple.txt présente la mise en forme.
+
