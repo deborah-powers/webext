0b0227f1673fca45da703a84743d0988635e252b
deborah-powers <deborah.powers.dev@gmail.com>
2025-06-05 21:26:17 +0200

    maison 06/05 21:26

--- all-clean-article/service-worker-v2.js
+++ all-clean-article/service-worker-v2.js
@@ -1,10 +1,3 @@
-function coco (n1, n2){
-	console.log ('coco', n1, n2);
-	n2.surnom = 'hello';
-}
-function dodo (n2){
-	console.log ('dodo', n2);
-}
 chrome.browserAction.onClicked.addListener (function (tab){
 	if (tab.url.includes ('https://osmose.numerique.gouv.fr/jcms/')){
 		// mission pour récupérer les pages html de la nouvelle spec et les mettre dans un word
@@ -14,7 +7,7 @@ chrome.browserAction.onClicked.addListener (function (tab){
 	});}
 	else if (! tab.url.includes ('http://localhost:1407') && 'http' === tab.url.substring (0,4) || 'file:///C:/' === tab.url.substring (0,11)){
 		chrome.scripting.executeScript ({
-			target: {tabId: tab.id, allFrames: false },
+			target: {tabId: tab.id, allFrames: true },
 			files: [ 'import-perso.js', 'cleanArticle.js', 'cleanAction.js' ]
 		});
 }});
