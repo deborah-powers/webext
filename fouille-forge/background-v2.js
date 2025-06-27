3e9a58f8d3a0243da2423fd1a905a272c867364d
deborah-powers <deborah.powers.dev@gmail.com>
2025-06-27 18:23:43 +0200

    travail 06/27 18:23

--- fouille-forge/background-v2.js
+++ fouille-forge/background-v2.js
@@ -1,5 +1,18 @@
 function notLaunch(){ console.log ("l'extension fouille forge ne peut pas s'executer sur cette page"); }
+/*
+https://stackoverflow.com/questions/4212861/what-is-a-correct-mime-type-for-docx-pptx-etc
+*/
 function cleanpage(){
+	HTMLAnchorElement.prototype.isDownload = function(){
+		if (! this.href.includes ('/download/')) return this.href;
+		var xhttp = new XMLHttpRequest();
+		// méthode synchrone
+		xhttp.open ('GET', this.href, false);
+		xhttp.setRequestHeader ('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+		xhttp.send();
+		console.dir (xhttp);
+		return this.href;
+	}
 	document.body.innerHTML = document.getElementsByClassName ('content')[0].innerHTML;
 	const pathAriana = document.getElementsByTagName ('td')[0].innerText.substring (10);
 	document.body.innerHTML = document.getElementsByTagName ('ul')[1].outerHTML;
@@ -7,7 +20,8 @@ function cleanpage(){
 	var finalText = "<p>Fil d'Arianne: "+ pathAriana + '</p><p>Url: '+ window.location.href + '</p><table>';
 	const linkRef = "<tr><td><a href=''></a><td></td></tr>";
 	for (var contain of containers){
-		const locRef = contain.getElementsByTagName ('a')[0].href;
+	//	const locRef = contain.getElementsByTagName ('a')[0].href;
+		const locRef = contain.getElementsByTagName ('a')[0].isDownload();
 		var linkLoc = linkRef.replace ("''", "'"+ locRef +"'");
 		linkLoc = linkLoc.replace ('></a', '>'+ contain.getElementsByTagName ('span')[0].innerText +'</');
 		linkLoc = linkLoc.replace ('td></td', 'td>'+ locRef +'</td');
