5a8d82e1b1b19db526aa5b19d6f4e7a3d704d386
deborah-powers <deborah.powers.dev@gmail.com>
2025-12-28 17:41:46 +0100

    maison 12/28 17:41

--- all-clean-article va/cleanEnd.js
+++
@@ -1,9 +0,0 @@
-fanfic.cleanTitle();
-libHtml.cleanBody();
-// libHtml.delAttributes();
-document.body.removeAnnotations();
-fanfic.fillHeader();
-addCss ([ 'structure', 'perso' ]);
-fanfic.text = document.body.innerHTML.replaceAll ('> ','>');
-var downloadText = fanfic.fillDownloadFile();
-libHtml.downloadFile (fanfic.title +'.html', downloadText);
