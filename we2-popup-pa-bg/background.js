ff2185ed5dc0cc0827db3761d2d004e1c9076b73
Deborah POWERS <deborah.powers.dev@gmail.com>
2026-02-20 12:02:08 +0100

    Update background.js

--- we2-popup-pa-bg/background.js
+++ we2-popup-pa-bg/background.js
@@ -1,4 +1,4 @@
-function avancer(){
+function avancerNavigateurQwant(){
 	console.log ('avancer');
 	var buttons = document.getElementsByClassName ('vE9rz jdx48 i89U4 SWHl4 _1xQVj uorL9 G-2qb');
 	var buttonMore = null;
@@ -8,11 +8,19 @@ function avancer(){
 	}
 	else{
 		buttonMore.click();
-		setTimeout (function(){ avancer(); }, 1000);
+		setTimeout (function(){ avancerNavigateurQwant(); }, 1000);
 	}
 }
+// recevoir un message du script de la popup et lui renvoyer une réponse
 chrome.runtime.onMessage.addListener (function (message, sender, sendResponse){
-	console.log ('action:', message.action);
-	avancer();
+	console.log ('action:', message.action, sender);
+	avancerNavigateurQwant();
 	sendResponse ({ status: 'hellove' });
 });
+// recevoir un message d'une autre extension et lui renvoyer une réponse
+chrome.runtime.onMessageExternal.addListener (function (message, sender, sendResponse){
+	console.log ("action d'une autre extension:", message.action, sender);
+	avancerNavigateurQwant();
+	sendResponse ({ status: 'hellove' });
+});
+// "externally_connectable": { "ids": ["*"] },
