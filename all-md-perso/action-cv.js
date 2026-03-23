92dfcf993d300c2a56ddc33c1323ea530e8c084f
deborah-powers <deborah.powers.dev@gmail.com>
2026-03-23 11:15:01 +0100

    travail 03/23 11:15

--- all-md-perso/action-cv.js
+++ all-md-perso/action-cv.js
@@ -3,6 +3,13 @@ const cvTemplateTxt = 'template-cv.txt';
 const formationTemplate = "<p>$date: <a href='$lien'>$titre</a></p>";
 const experienceTemplate = "<div><p>$date<br/>$client</p><h3>$titre</h3><p>$description</p></div>";
 
+crutialData =`
+	strip: function (text, char){ return text.strip (char); },
+	fromModel: function (text, model){ return text.fromModel (model); },
+	toModel: function (text, dataDict){ return text.toModel (dataDict); }
+`;
+const htmlLib = callLibrary ([ 'textFct' ]);
+
 function openWEfile (fileName){
 	// fileName est dans le dossier de l'extension
 	const filePath = chrome.extension.getURL (fileName);
@@ -18,89 +25,58 @@ var htmlTemplate = openWEfile (cvTemplateHtml);
 const cvData ={
 	metier: "", metierFemminin: "", dureeExperience: "", presentation: "",
 	competences: "", formations: "", outils: "", experiences: "", projetsPerso: "",
+
 	fillTemplate: function (template){
 		// les lignes simples
 		template = template.replace ('$metierFemminin', this.metierFemminin);
 		template = template.replaceAll ('$metier', this.metier);
-		template = template.replace ('$dureeExperience', this.dureeExperience);
-		template = template.replace ('$presentation', this.presentation);
+		var dataDict ={};
+		dataDict ['dureeExperience'] = this.dureeExperience;
+		dataDict ['presentation'] = this.presentation;
 		// les blocs
 		// les compétences
 		this.competences = this.competences.replaceAll ('\n', '</p><p>');
 		this.competences = '<p>' + this.competences + '</p>';
-		template = template.replace ('$competences', this.competences);
+		dataDict ['competences'] = this.competences;
 		// les formations
 		var sectionList = this.formations.split ('\n');
+		this.formations ="";
 		var itemText ="";
 		for (var s=0; s< sectionList.length; s++){
 			sectionList[s] = sectionList[s].split ('\t');
 			itemText = formationTemplate.replace ('$date', sectionList[s][0]);
 			itemText = itemText.replace ('$titre', sectionList[s][1]);
 			itemText = itemText.replace ('$lien', sectionList[s][2]);
+			this.formations = this.formations + itemText;
 		}
-		template = template.replace ('$formations', this.formations);
+		dataDict ['formations'] = this.formations;
 		// les outils
 		this.outils = this.outils.replaceAll (': ', '</h3><p>');
 		this.outils = this.outils.replaceAll ('\n', '</p><h3>');
 		this.outils = '<h3>' + this.outils + '</p>';
-		template = template.replace ('$outils', this.outils);
-		// les expériences
-		template = template.replace ('$experiences', this.experiences);
-		// les projets personnels
-		template = template.replace ('$projetsPerso', this.projetsPerso);
+		dataDict ['outils'] = this.outils;
+		dataDict ['experiences'] = this.experiences;
+		dataDict ['projetsPerso'] = this.projetsPerso;
+		template = htmlLib.toModel (template, dataDict);
 		return template;
 	},
-	extractData_va: function (textSrc){
-		while (textSrc.includes ('\n\n')) textSrc = textSrc.replaceAll ('\n\n', '\n');
-		// les lignes simples
-		var d=8+ textSrc.indexOf ('Métier: ');
-		var f= textSrc.indexOf ('\n',d);
-		this.metier = textSrc.substring (d,f);
-		d=10+ textSrc.indexOf ('Fémminin: ');
-		f= textSrc.indexOf ('\n',d);
-		this.metierFemminin = textSrc.substring (d,f);
-		d=12+ textSrc.indexOf ('Expérience: ');
-		f= textSrc.indexOf ('\n',d);
-		this.dureeExperience = textSrc.substring (d,f);
-		d=14+ textSrc.indexOf ('Presentation: ');
-		f= textSrc.indexOf ('\n',d);
-		this.presentation = textSrc.substring (d,f);
-		// les blocs
-		d=16+ textSrc.indexOf ('\n== Compétences\n');
-		f= textSrc.indexOf ('\n==',d);
-		this.competences = textSrc.substring (d,f);
-		d=11+ textSrc.indexOf ('\n== Outils\n');
-		f= textSrc.indexOf ('\n==',d);
-		this.outils = textSrc.substring (d,f);
-		d=15+ textSrc.indexOf ('\n== Formations\n');
-		f= textSrc.indexOf ('\n==',d);
-		this.formations = textSrc.substring (d,f);
-		d=16+ textSrc.indexOf ('\n== Experiences\n');
-		f= textSrc.indexOf ('\n==',d);
-		this.experiences = textSrc.substring (d,f);
-	},
 	extractData: function (textSrc, templateSrc){
-		var templateList = templateSrc.split ('$');
-		// récupérer les données
-		for (var t=0; t< templateList.length; t=t+2) textSrc = textSrc.replace (templateList[t], "$");
-		console.log (textSrc);
-		// récupérer le nom des blocs
-		var t= templateList.length -1;
-		for (; t>=0; t=t-2) templateList.splice (t,1);
-		// transformer le tout en dictionnaire
-		var textList = textSrc.split ('$');
-		textList.splice (0,1);
-		textList.splice (textList.length -1, 1);
-		var dataDict ={};
-		for (var d=0; d< textList.length; d++) dataDict[templateList[d]] = textList[d];
-		console.log (dataDict);
+		const dataDict = htmlLib.fromModel (textSrc, templateSrc);
+		this.metier = dataDict['metier'];
+		this.metierFemminin = dataDict['metierFemminin'];
+		this.dureeExperience = dataDict['dureeExperience'];
+		this.presentation = dataDict['presentation'];
+		this.competences = dataDict['competences'];
+		this.formations = dataDict['formations'];
+		this.outils = dataDict['outils'];
+		this.experiences = dataDict['experiences'];
+		this.projetsPerso = dataDict['projetsPerso'];
 	}
 };
 const templateSrc = openWEfile (cvTemplateTxt);
 var textSrc = document.body.innerText.replaceAll ('\n\n', '\n');
 while (textSrc.includes ('\n\n')) textSrc = textSrc.replaceAll ('\n\n', '\n');
 cvData.extractData (textSrc, templateSrc);
-/*
+
 htmlTemplate = cvData.fillTemplate (htmlTemplate);
 document.body.innerHTML = htmlTemplate;
-*/
