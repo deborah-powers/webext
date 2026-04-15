0cf383d5bea1e473bf09e3dc4af20d3768f4b238
deborah-powers <deborah.powers.dev@gmail.com>
2026-04-15 16:18:35 +0200

    travail 04/15 16:18

--- dila-biblio/background va.js
+++ dila-biblio/background va.js
@@ -21,8 +21,31 @@ HTMLInputElement.prototype.mimicTyping_va = function (message){ for (var letter 
 HTMLInputElement.prototype.mimicTyping_vc = function (message){
 	this.value = message;
 	this.dispatchEvent (new KeyboardEvent ('keydown', { key: 'e', keyCode: 69, code: 'KeyE', which: 69 }));
+//	this.dispatchEvent (new KeyboardEvent ('keydown', { charCode: 109, code: 'Semicolon', keyCode: 109, key: 'm', type: 'keypress', which: 109, composed: true, returnValue: true, bubbles: true, isTrusted: true }));
 }
 
+HTMLElement.prototype.enter = function(){
+	var keyboardEvent = new KeyboardEvent ('keydown', { code: 'Enter', key: 'Enter', charKode: 13, keyCode: 13, view: window });
+	this.dispatchEvent (keyboardEvent);
+}
+function simulateMouseClick (targetNode){
+	// https://stackoverflow.com/questions/24025165/simulating-a-mousedown-click-mouseup-sequence-in-tampermonkey
+	function triggerMouseEvent (targetNode, eventType){
+		var clickEvent = document.createEvent ('MouseEvents');
+		clickEvent.initEvent (eventType, true, true);
+		targetNode.dispatchEvent (clickEvent);
+	}
+	['mouseover', 'mousedown', 'mouseup', 'click'].forEach (function (eventType){ triggerMouseEvent (targetNode, eventType); });
+}
+function writeMessage (input, message){
+	setTimeout (function(){
+	console.log (message);
+	input.value = input.value + message[0];
+	input.setAttribute ('value', input.getAttribute ('value') + message[0]);
+	simulateMouseClick (this);
+	if (message.length >1) writeMessage (input, message.substring (1));
+}, 500);
+}
 var buttonNext = document.getElementById ('btn-next');
 function chooseActionOverPage(){
 	buttonNext = document.getElementById ('btn-next');
--- dila-biblio/background.js
+++ dila-biblio/background.js
@@ -2,44 +2,107 @@
 var inputMessage ="";
 var imId =0;
 HTMLInputElement.prototype.mimicTyping = function (message){
-	this.focus();
 	this.value = message;
-	this.click();
-}
-HTMLInputElement.prototype.mimicTyping_va = function (message){
-	this.value = message;
-	this.dispatchEvent (new KeyboardEvent ('keydown', { key: 'e', keyCode: 69, code: 'KeyE', which: 69 }));
+	this.dispatchEvent (new KeyboardEvent ('keydown',{ key: 'e', keyCode: 69, code: 'KeyE', which: 69 }));
 }
 HTMLElement.prototype.findByInnerText = function (message){
 	if (this.innerText.includes (message)){
-		var res = null;
+		var tagRes = null;
 		var c=0;
-		while (c< this.children.length && res === null){
-			res = this.children[c].findByInnerText (message);
+		while (c< this.children.length && tagRes === null){
+			tagRes = this.children[c].findByInnerText (message);
 			c+=1;
 		}
-		if (res === null) return this;
-		else return res;
+		if (tagRes === null) return this;
+		else return tagRes;
 	}
 	else return null;
 }
-HTMLElement.prototype.findNextInput = function (label){
-	var textTag = this.findByInnerText (label);
-	if (textTag === null) return null;
-	while (! textTag.innerHTML.includes ('<input ')) textTag = textTag.parentElement;
-	textTag = textTag.getElementsByTagName ('input')[0];
-	return textTag;
+// trouver les inputs
+HTMLElement.prototype.findLabelByInnerText = function (message){
+	if (! this.innerText.includes (message)) return null;
+	else if (this.tagName === 'LABEL') return this;
+	else{
+		var label = null;
+		var c=0;
+		const labels = this.getElementsByTagName ('label');
+		while (c< labels.length && label === null){
+			if (labels[c].innerText.includes (message)) label = labels[c];
+			c+=1;
+		}
+		c=0;
+		while (c< this.children.length && label === null){
+			label = this.children[c].findLabelByInnerText (message);
+			c+=1;
+		}
+		if (label === null) return this;
+		else return label;
+}}
+HTMLElement.prototype.findNextInput = function (message){
+	const label = this.findLabelByInnerText (message);
+	if (label === null) return null;
+	else{
+		const inputId = label.getAttribute ('for');
+		var input = null;
+		if (inputId === undefined || inputId === null){
+			input = label.parentElement.parentElement.getElementsByTagName ('input')[0];
+			if (input === null || input === undefined) input = label.parentElement.parentElement.getElementsByTagName ('select')[0];
+			if (input === null || input === undefined) input = label.parentElement.parentElement.getElementsByTagName ('textarea')[0];
+		}
+		else input = document.getElementById (inputId);
+		console.log (message, input);
+		return input;
+}}
+HTMLInputElement.prototype.fillInput = function (message){
+	const inputType = this.getAttribute ('type');
+	// pour les text
+	this.setAttribute ('value', message);
+	this.value = message;
+	var changeEvent = new Event ('change', { bubbles: true });
+	this.dispatchEvent (changeEvent);
+}
+HTMLSelectElement.prototype.fillInput = function (message){
+	const options = this.getElementsByTagName ('option');
+	var o=0;
+	while (o< options.length){
+		if (options[o].innerText.includes (message)){
+			options[o].selected = true;
+			this.selectedIndex = options[o].index;
+			this.value = options[o].value;
+			o= options.length;
+		}
+		o+=1;
+}}
+function fillInputByLabel (labelText, message){
+	var input = document.body.findNextInput (labelText);
+	if (input === null || input === undefined) console.log ("pas d'input pour", labelText);
+	else input.fillInput (message);
+//	console.log ('fillInputByLabel', labelText, input.value, input.getAttribute ('value'));
 }
 function chooseActionOverPage(){
+	const titrePage = document.getElementById ('titrePage');
+	titrePage.removeAttribute ('role');
+	titrePage.removeAttribute ('aria-live');
+
+
 	const buttonNext = document.getElementById ('btn-next');
 	if (document.body.innerText.includes ('Étape 1') && document.body.innerText.includes ('Avertissement')){
 		buttonNext.click();
-		setTimeout (chooseActionOverPage, 3000);
+		setTimeout (chooseActionOverPage, 1500);
 	}
 	else if (document.body.innerText.includes ('Mes informations personnelles')){
-		// identifier la nationnalité
-		var nationnalite = document.body.findNextInput ('Nationalité');
-		console.log ('a', nationnalite, nationnalite.constructor.name);
+		fillInputByLabel ('Situation matrimoniale', 'Célibatair');
+		fillInputByLabel ('Profession', 'Direct');
+		fillInputByLabel ('Indicatif', '+33 (FRANCE');
+		fillInputByLabel ('Numéro de téléphone principal', '63298167');
+		fillInputByLabel ('Adresse électronique', 'moi@gmail.co');
+		fillInputByLabel ('Pays', 'FRANC');
+		fillInputByLabel ('Numéro et libellé de voie', '1 rue du lapi');
+		fillInputByLabel ('Code postal / Localité', 'ALLEVARD (38580');
 	}
-}HTMLLabelElement
-setTimeout (chooseActionOverPage, 3000);
+	else if (document.body.innerText.includes ('Description des faits')) fillInputByLabel ('Date', '2026-04-13');
+	else if (document.body.innerText.includes ('Préjudice financier')){
+		fillInputByLabel ('Date', '2026-04-13');
+	}
+}
+setTimeout (chooseActionOverPage, 1500);
