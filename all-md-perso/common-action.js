94e0b30a585ea3d73f1f4d9f0f8df7c537d51004
deborah-powers <deborah.powers.dev@gmail.com>
2026-04-28 17:35:25 +0200

    travail 04/28 17:35

--- all-md-perso/common-action.js
+++ all-md-perso/common-action.js
@@ -15,39 +15,8 @@ var pageOriginale = document.body.innerText;
 const title = htmlLib.findTitle (window.location.href);
 htmlTemplate = htmlTemplate.replace ('<title></title>', '<title>' + title + '</title>');
 
-function findMetaLocal_va (metadata, title){
-	// créer la page
-	htmlTemplate = htmlTemplate.replaceAll ('$titre', title);
-	if (""=== metadata['lien']){
-		htmlTemplate = htmlTemplate.replace ("<a href='$lien'>", "");
-		htmlTemplate = htmlTemplate.replace ('</a>', "");
-	}
-	else htmlTemplate = htmlTemplate.replace ('$lien', metadata['lien']);
-	if (! htmlLib.exists (metadata['autlink'])){
-		htmlTemplate = htmlTemplate.replace ("<a href='$lienAuteur'>", "");
-		htmlTemplate = htmlTemplate.replace ('$auteur</a>', '$auteur');
-	}
-	else htmlTemplate = htmlTemplate.replace ('$lienAuteur', metadata['autlink']);
-	htmlTemplate = htmlTemplate.replace ('$auteur', metadata['auteur']);
-	htmlTemplate = htmlTemplate.replace ('$sujet', metadata['sujet']);
-	// les autres métadonnées
-	const metaTemplate = '<p>$key: $value</p>\n\t$text';
-	var metaText ="";
-	for (var m in metadata) if (! [ 'auteur', 'sujet', 'lien' ].includes (m)){
-		metaText = metaTemplate.replace ('$key', m);
-		metaText = metaText.replace ('$value', metadata[m]);
-		htmlTemplate = htmlTemplate.replace ('$text', metaText);
-	}
-	// le lien de téléchargement
-	htmlTemplate = htmlTemplate.replace ('$text', document.body.innerHTML);
-	var textEncoded = encodeURIComponent (htmlTemplate);
-	textEncoded = textEncoded.replaceAll ("'", '%27');
-	htmlTemplate = htmlTemplate.replace ('$data', textEncoded);
-	document.body.innerHTML = htmlTemplate.sliceWords ('<body>', '</body>');
-	document.head.innerHTML = htmlTemplate.sliceWords ('<head>', '</head>');
-}
 function findMetaLocal (metadata, title){
-	// créer la page
+	// les principales métadonnées
 	htmlTemplate = htmlTemplate.replaceAll ('$titre', title);
 	if (""=== metadata['lien']){
 		htmlTemplate = htmlTemplate.replace ("<a href='$lien'>", "");
@@ -70,13 +39,14 @@ function findMetaLocal (metadata, title){
 		htmlTemplate = htmlTemplate.replace ('$meta', metaText);
 	}
 	htmlTemplate = htmlTemplate.replace ('$meta', "");
-	// le lien de téléchargement
+	// créer la page
 	htmlTemplate = htmlTemplate.replace ('$text', document.body.innerHTML);
+	// le lien de téléchargement
 	var textEncoded = encodeURIComponent (htmlTemplate);
 	textEncoded = textEncoded.replaceAll ("'", '%27');
 	htmlTemplate = htmlTemplate.replace ('$data', textEncoded);
 	document.body.innerHTML = htmlTemplate.sliceWords ('<body>', '</body>');
-	document.head.innerHTML = htmlTemplate.sliceWords ('<head>', '</head>');
+	document.head.innerHTML = htmlTemplate.sliceWords ('<head>', '</head>') + document.head.innerHTML;
 }
 const metadata = htmlLib.prepareText();
 findMetaLocal (metadata, title);
